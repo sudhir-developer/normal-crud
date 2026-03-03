b784536dee64bb3de0fc17fd11f131b15c007977
sudhir-developer <sudhirkumarpatel1810@gmail.com>
2026-03-03 10:29:42 +0530

    Product loading on page scroll using throttle

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -13,6 +13,7 @@ function AboutPage(){
 
 const [users, setUsers] = useState<User[]>([]);
 const [loading, setLoading] = useState(false);
+const [visibleCount, setVisibleCount] = useState(16);
 
 useEffect(()=>{
 
@@ -35,6 +36,33 @@ useEffect(()=>{
 
 },[])
 
+function throttle(func:any, delay:number) {
+  let lastCall = 0;
+  return function (...args:any) {
+    const now = new Date().getTime();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+
+ // Scroll listener
+ useEffect(() => {
+  const handleScroll = throttle(() => {
+    const nearBottom =
+      window.innerHeight + window.scrollY >=
+      document.documentElement.scrollHeight - 200;
+    if (nearBottom && visibleCount < users.length) {
+      setVisibleCount((prev) => prev + 4);
+    }
+  },100);
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+}, [visibleCount, users.length]);
+// Scroll listener
+
+
 const handleToggle = (id: number) => {
   setUsers((prev) =>prev.map((item) =>item.id === id? { ...item, completed: !item.completed }: item)
   );
@@ -44,14 +72,28 @@ const handleToggle = (id: number) => {
     return(
        <>
        {loading && <p>Loading...</p>}
-       {!loading && <>
-       <ul>
-       {users?.map((user)=>(
-           <li key={user.id}>{user.title}-<b>{user.completed? "True":"False"} <input type="checkbox" checked={user.completed} onChange={() => handleToggle(user.id)}/></b></li>
-       ))}
-      </ul>
-      </>
-      }
+       {!loading && (
+        <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-4 p-6">
+          {users.slice(0, visibleCount).map((user) => (
+            <div
+              key={user.id}
+              className="rounded-[30px] bg-gray-300 hover:bg-blue-400 transition text-black p-6"
+            >
+              {user.title} -
+              <b>
+                {user.completed ? "True" : "False"}
+                <input
+                  type="checkbox"
+                  checked={user.completed}
+                  onChange={() => handleToggle(user.id)}
+                />
+              </b>
+            </div>
+          ))}
+        </div>
+        </div>
+      )}
        </>
     )
    
